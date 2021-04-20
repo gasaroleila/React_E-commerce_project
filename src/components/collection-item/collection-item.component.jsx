399f719165e2e28d18893cabc708554c2495c467
gasaroleila@gmail.com <uwamgaleila@gmail.com>
2021-04-20 13:14:23 +0300

    created cart-item component and connected our cart dropdown with cartItems

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addItem } from '../../redux/cart/cart.action'
+import CustomButton from '../custom-button/custom-button.component'
 
 import './collection-item.styles.scss'
 
-const CollectionItem = ({id,imageUrl,name, price})=> (
+const CollectionItem = ({item,addItem})=> {
+    const { imageUrl,name, price } = item
+    return (
     <div className="collection-item">
         <div className='background-image' style={{
             backgroundImage: `url(${imageUrl})`
@@ -11,7 +16,14 @@ const CollectionItem = ({id,imageUrl,name, price})=> (
             <p>{name}</p>
             <p className='last'>{price}</p>
         </div>
-    </div>
-)
 
-export default CollectionItem
+        <CustomButton inverted onClick= {()=> addItem(item)}>Add to Cart</CustomButton>
+    </div>
+)}
+
+const mapDispatchToProps = dispatch=> ({
+    addItem: item=> dispatch(addItem(item))
+}) 
+
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
